a93dcb8e12ef04d5a724813f6985c4814cf36caa
Peter Potrowl <peter.potrowl@gmail.com>
2016-03-14 14:41:46 +0100

    Add form type with Date validation.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints\Date;
 
 class DefaultController extends Controller
 {
@@ -13,9 +15,29 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $form = $this->createFormBuilder(null, [
+                'csrf_protection' => false,
+                'method' => 'GET',
+            ])
+            ->add('date', DateType::class, [
+                'label' => 'Date test:',
+                'widget' => 'single_text',
+                'constraints' => [
+                    new Date(),
+                ],
+                'attr' => [
+                    'placeholder' => 'yyyy-mm-dd',
+                ],
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+        $date = $form->isValid() ? $form->get('date')->getData() : null;
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'form' => $form->createView(),
+            'date' => $date,
         ]);
     }
 }
